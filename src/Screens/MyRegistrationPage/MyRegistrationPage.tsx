35afd00f46e6e95590eca0bc72d12459ad99874f
Adhil-M <68767389+Adhil-M@users.noreply.github.com>
2021-03-04 13:30:53 +0530

    First commit2

--- src/Screens/MyRegistrationPage/MyRegistrationPage.tsx
+++ src/Screens/MyRegistrationPage/MyRegistrationPage.tsx
@@ -1,33 +1,49 @@
 import React, { useState } from "react";
 import Header from '../../Components/Header/Header';
 import UserEvents from "../../data1";
-import { Upload, Button, message, Form ,Input } from "antd";
+import { Upload, Button, message, Form, Input } from "antd";
 import useFitText from "use-fit-text";
-import { EditOutlined, UploadOutlined ,CloseOutlined,PlusOutlined,MinusOutlined } from "@ant-design/icons";
-import {useParams,Link} from 'react-router-dom';
+import { EditOutlined, UploadOutlined, CloseOutlined, PlusOutlined, MinusCircleOutlined } from "@ant-design/icons";
+import { useParams, Link } from 'react-router-dom';
 interface ParamTypes {
   eId: string
 }
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 4 },
+  },
+  wrapperCol: {
+    xs: { span: 24, offset: 0 },
+    sm: { span: 14, offset: 0 },
+  },
+};
+const formItemLayoutWithOutLabel = {
+  wrapperCol: {
+    xs: { span: 24, offset: 0 },
+    sm: { span: 14, offset: 4 },
+  },
+};
 
 
 var j = 0;
 export default function MyRegistationPage() {
-//   const [fileList, setFilelist] = useState<any[]>();
-//   const [uploading, setUploading] = useState(false);
-//   function handleUpload() {
-//     const formData = new FormData();
+  //   const [fileList, setFilelist] = useState<any[]>();
+  //   const [uploading, setUploading] = useState(false);
+  //   function handleUpload() {
+  //     const formData = new FormData();
 
-//     setFilelist(() => {
-//       if (fileList && formData)
-//         fileList.forEach((file) => {
-//           formData.append("files[]", file);
-//         });
+  //     setFilelist(() => {
+  //       if (fileList && formData)
+  //         fileList.forEach((file) => {
+  //           formData.append("files[]", file);
+  //         });
 
-//       return fileList;
-//     });
+  //       return fileList;
+  //     });
 
-//     setUploading(true);
-//   }
+  //     setUploading(true);
+  //   }
   let { eId } = useParams<ParamTypes>();
   type SizeType = Parameters<typeof Form>[0]['size'];
   const [componentSize, setComponentSize] = useState<SizeType | 'default'>('default');
@@ -35,19 +51,34 @@ export default function MyRegistationPage() {
     setComponentSize(size);
   };
   var i;
-  for(i=0;i<UserEvents.length;i++){
-    if(eId.localeCompare(UserEvents[i]["name"])==0)
+  for (i = 0; i < UserEvents.length; i++) {
+    if (eId.localeCompare(UserEvents[i]["name"]) == 0)
       break;
   }
   const { fontSize, ref } = useFitText({ maxFontSize: 500, minFontSize: 100 });
   console.log(fontSize);
-  const [ isEdit,setisEdit ]=useState(false);
-  var [leader ,setLeader]=useState(UserEvents[i]["team"]["leader"]);
-  
+  const [isEdit, setisEdit] = useState(false);
+  var [leader, setLeader] = useState(UserEvents[i]["team"]["leader"]);
+  var [members, setMember] = useState(UserEvents[i]["team"]["members"]);
+  function updateMember(index: number, e: React.ChangeEvent<HTMLInputElement>) {
+    let newarr = [...members];
+    newarr[index] = e.target.value
+    setMember(newarr);
+  }
+  function deleteMember(index: number) {
+    let newarr = [...members];
+    for (index; index < members.length; index++) {
+      newarr[index] = newarr[index + 1];
+      newarr.pop();
+    }
+
+    setMember(newarr);
+  }
+
   return (
     <div>
-    <Header mainText={"Event "+ eId} showBack={true}/>
-      <div className="myreg-status"  ref={ref} style={{ fontSize }}>
+      <Header mainText={"Event " + eId} showBack={true} />
+      <div className="myreg-status" ref={ref} style={{ fontSize }}>
         <p className="myreg-bold" >Registration Status: </p>
         <p>{UserEvents[i]["status"]} </p>
       </div>
@@ -55,7 +86,7 @@ export default function MyRegistationPage() {
         <h6>Announcement</h6>
         <p>{UserEvents[i]["announcement"]}</p>
       </div>
-      {!isEdit?<div className="team" ref={ref} style={{ fontSize }}>
+      {!isEdit ? <div className="team" ref={ref} style={{ fontSize }}>
         <h6>Team Details</h6>
         <h5>Leader</h5>
         <p className="teammembers">{UserEvents[i]["team"]["leader"]}</p>
@@ -63,10 +94,10 @@ export default function MyRegistationPage() {
         {UserEvents[i]["team"]["members"].map((name) => (
           <p className="teammembers">{name}</p>
         ))}
-        <Button className="button" onClick={()=>setisEdit(true)} icon={<EditOutlined />} />
-      </div>:
+        <Button className="button" onClick={() => setisEdit(true)} icon={<EditOutlined />} />
+      </div> :
         <div className="team" ref={ref} style={{ fontSize }}>
-          <Button shape='circle' onClick={()=>setisEdit(false)} icon={<CloseOutlined />} />
+          <Button shape='circle' onClick={() => setisEdit(false)} icon={<CloseOutlined />} />
           <Form
             labelCol={{ span: 4 }}
             wrapperCol={{ span: 14 }}
@@ -76,10 +107,40 @@ export default function MyRegistationPage() {
             size={componentSize as SizeType}
           >
             <Form.Item label="Leader">
-              <Input value={ leader } onChange={(e)=>setLeader(e.target.value)} />
+              <Input value={leader} onChange={(e) => setLeader(e.target.value)} />
             </Form.Item>
+            {
+              members.map((member, index) => (
+                <Form.Item {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)} label={index === 0 ? 'Members' : ''}>
+                  <Input style={{ display: "inline-block" }} key={index} value={member} accessKey={index.toString()} onChange={(e) => updateMember(parseInt(e.target.accessKey), e)} />
+                  {index > 0 && <MinusCircleOutlined style={{ display: "inline-block" }}
+                    accessKey={index.toString()}
+                    className="dynamic-delete-button"
+                    onClick={(e) => deleteMember(index)}
+                  />}
+                </Form.Item>
+              ))
+            }
+            <Button
+              type="dashed"
+              onClick={() => setMember([...members, members[members.length] = ''])}
+              style={{ width: '40%' }}
+              icon={<PlusOutlined />}
+            >
+              Add Member
+              </Button>
+
+            <Button 
+              type="dashed"
+              onClick={() => null}
+              style={{ width: '40%' ,display:"block",margin:'auto' }}
+             
+            >
+              Submit
+              </Button>
+
           </Form>
-        </div>
+        </div >
       }
       {/* need to do it later!!
        <div className="submission">
@@ -97,7 +158,7 @@ export default function MyRegistationPage() {
           {uploading ? "Uploading" : "Start Upload"}
         </Button>
       </div> */}
-    </div>
+    </div >
   );
 }
 
