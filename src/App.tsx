e049b891c090b3b8401ab1fe9a03f4a8b882aaf8
Vimal Joseph <vimaljoseph027@gmail.com>
2021-04-05 20:21:55 +0530

    redirection

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import HomePage from "./Screens/HomePage/HomePage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import EventsPage from "./Screens/EventsPage/EventsPage";
 import GoogleAuthCallback from "./Screens/GoogleAuthCallback";
+import RedirectPage from "./Screens/RedirectPage";
 import { backendURI, Cats, onLogout } from "./data";
 import dayjs from "dayjs";
 import EditProfile from "./Screens/ProfilePage/EditProfile";
@@ -36,7 +37,7 @@ function App() {
     updated_at: "",
     ragamID: "",
     collegeName: "",
-    referralCode:"",
+    referralCode: "",
     gender: "",
     eventDetails: Array<{
       id: 0;
@@ -56,7 +57,7 @@ function App() {
       created_at: "",
       updated_at: "",
       ragamID: "",
-      referralCode:"",
+      referralCode: "",
       collegeName: "",
       gender: "",
       eventDetails: [],
@@ -72,7 +73,7 @@ function App() {
     fetch(backendURI + "categories")
       .then((response) => {
         if (response.ok) return response.json();
-        else message.error("Some error Occurred. Please try again later.")
+        else message.error("Some error Occurred. Please try again later.");
       })
       .then((json) => {
         for (let i in json)
@@ -111,10 +112,9 @@ function App() {
               setUser: setUser,
             });
             message.error("Session Expired! Please Login again.", 5);
-          }else if(result.statusCode){
+          } else if (result.statusCode) {
             message.error(result.message, 5);
-          }
-           else {
+          } else {
             console.log(user.token);
             setuserDetails(result);
             setCatloading(false);
@@ -139,6 +139,15 @@ function App() {
           </Route>
           <Route path="/auth/callback">
             <GoogleAuthCallback
+              categories={categories}
+              catLoading={catLoading}
+              userDetails={userDetails}
+              user={user}
+              setUser={setUser}
+            />
+          </Route>
+          <Route path="/redirect">
+            <RedirectPage
               categories={categories}
               catLoading={catLoading}
               userDetails={userDetails}
@@ -229,7 +238,6 @@ function App() {
               getUserEvents={getUserEvents}
               setUser={setUser}
               setUserDetails={setuserDetails}
-
             />
           </Route>
           <Route path="/editprofile">
@@ -241,11 +249,10 @@ function App() {
               getUserEvents={getUserEvents}
               setUser={setUser}
               setUserDetails={setuserDetails}
-              
             />
           </Route>
           <Route path="/login">
-          <LoginPage/>
+            <LoginPage />
           </Route>
         </Switch>
       </Router>
---
+++ src/Screens/RedirectPage.tsx
@@ -0,0 +1,31 @@
+import { message } from "antd";
+import React, { useRef, useState } from "react";
+import { useLocation, useHistory } from "react-router-dom";
+import { backendURI, PropTypes } from "../data";
+
+const RedirectPage = (props: PropTypes) => {
+  const location = useLocation();
+  const formRef = useRef<HTMLFormElement>(null);
+  React.useEffect(() => {
+    if (!location) {
+      return;
+    }
+    console.log(location.search);
+    if (formRef.current) {
+      formRef.current.submit();
+    }
+  }, [location, formRef]);
+  return (
+    <div>
+      <form
+        method="POST"
+        action={new URLSearchParams(location.search).get("url") || undefined}
+        ref={formRef}
+      >
+        <input type="hidden" name="token" value={props.user.token} />
+      </form>
+      <h4>Redirecting to :{new URLSearchParams(location.search).get("url")}</h4>
+    </div>
+  );
+};
+export default RedirectPage;
